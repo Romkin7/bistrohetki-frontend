099611d35c6bfce3acba9315b31003c4ad5d274d
Roman Tuomisto <romantuomisto@Mac.lan>
2025-10-20 19:38:20 +0300

    adds lunch menu page

---
+++ src/pages/LunchMenuPage.tsx
@@ -0,0 +1,41 @@
+import { Box, Flex, Grid, GridItem, Image } from "@chakra-ui/react";
+import { type FC } from "react";
+import { useLoaderData } from "react-router";
+import Heading from "@/shared/Heading/Heading";
+import type { LunchMenuPageData } from "@/zod/pages/lunchMenuPageData";
+
+const LunchMenuPage: FC = () => {
+  const lunchMenuPageData: LunchMenuPageData = useLoaderData();
+
+  return (
+    <section>
+      <Flex direction="column" align="center" justify="center" mb={8}>
+        <Heading
+          tag="h1"
+          variant="title-1"
+          color="dark"
+          ariaLabel={lunchMenuPageData?.mainTitle}
+        >
+          {lunchMenuPageData?.mainTitle}
+        </Heading>
+      </Flex>
+      <Grid
+        templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(4, 1fr)" }}
+        gap={4}
+      >
+        <GridItem key={lunchMenuPageData?.menu.id}>
+          <Box key={lunchMenuPageData?.menu.id}>
+            <Image
+              key={lunchMenuPageData?.menu.id}
+              src={lunchMenuPageData?.menu.url}
+              alt={lunchMenuPageData?.menu.alternativeText}
+              className="rounded-lg"
+            />
+          </Box>
+        </GridItem>
+      </Grid>
+    </section>
+  );
+};
+
+export default LunchMenuPage;
